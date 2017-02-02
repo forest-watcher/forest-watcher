5c9eac62151d58be3be1dbab60b5fadab70d8777
j8seangel <jseangel@gmail.com>
2017-02-02 19:54:01 +0100

    Setup reducer

--- app/redux-modules/index.js
+++ app/redux-modules/index.js
@@ -2,3 +2,4 @@ export { default as navigation } from './navigation';
 export { default as alerts } from './alerts';
 export { default as user } from './user';
 export { default as countries } from './countries';
+export { default as setup } from './setup';
