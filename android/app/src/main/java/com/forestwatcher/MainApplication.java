a5f5d972fd814c6d30a57cd2fc33205d4ee434d8
Gerardo <gerardo.pacheco@vizzuality.com>
2017-02-08 10:07:13 +0100

    Fix: Android - React Native i18n Link

--- android/app/src/main/java/com/forestwatcher/MainApplication.java
+++ android/app/src/main/java/com/forestwatcher/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new ReactNativeConfigPackage(),
-          new MapsPackage()
+          new MapsPackage(),
+          new ReactNativeI18n()
       );
     }
   };
