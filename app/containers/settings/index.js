9b64a9d45771f615393ae538d866991b7293c36c
j8seangel <jseangel@gmail.com>
2017-02-14 13:48:44 +0100

    Use own navigation prop in containers

--- app/containers/settings/index.js
+++ app/containers/settings/index.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import { navigatePush, navigatePop } from 'redux-modules/navigation';
 import { getAreas } from 'redux-modules/areas';
 
 import Settings from 'components/settings';
@@ -14,12 +13,6 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    navigate: (action) => {
-      dispatch(navigatePush(action));
-    },
-    navigateBack: (action) => {
-      dispatch(navigatePop(action));
-    },
     getAreas: () => {
       dispatch(getAreas());
     }
