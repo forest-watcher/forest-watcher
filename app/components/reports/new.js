843150ad29b635ad553e26e9d5da509c1bb2397d
j8seangel <jseangel@gmail.com>
2017-02-15 19:45:39 +0100

    Get custom btn message

--- app/components/reports/new.js
+++ app/components/reports/new.js
@@ -9,6 +9,19 @@ import getInputForm from 'components/common/form-inputs';
 import ActionButton from 'components/common/action-button';
 import styles from './styles';
 
+function getBtnTextByType(type) {
+  switch (type) {
+    case 'text':
+      return 'Please write something';
+    case 'radio':
+      return 'Choose one option to continue';
+    case 'select':
+      return 'Choose at least one option to continue';
+    default:
+      return 'Please, fill the answer please';
+  }
+}
+
 class Reports extends Component {
   constructor() {
     super();
@@ -31,20 +44,21 @@ class Reports extends Component {
   render() {
     const { questions, answers } = this.props;
     if (!questions || !questions.length) return null;
-    const enabled = answers && questions.length && answers[questions[this.state.page]._id] // eslint-disable-line
-    const btnText = enabled ? 'Next' : 'Choose at least one option to continue';
+    const question = questions[this.state.page];
+    const enabled = answers && answers[question._id];  // eslint-disable-line
+    const btnText = enabled ? 'Next' : getBtnTextByType(question.type);
     return (
       <View style={styles.container}>
         <StepsSlider
           page={this.state.page}
           onChangeTab={this.updatePage}
         >
-          {questions.map((question, index) => (
+          {questions.map((item, index) => (
             <View key={index}>
               <Field
-                name={question._id} // eslint-disable-line
+                name={item._id} // eslint-disable-line
                 component={getInputForm}
-                question={question}
+                question={item}
               />
             </View>
           ))}
