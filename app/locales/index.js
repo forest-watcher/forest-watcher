2e47887f1ad8d3967a24e9603ffb0ea79a27c632
j8seangel <jseangel@gmail.com>
2017-02-24 17:07:58 +0100

    Include new languages

--- app/locales/index.js
+++ app/locales/index.js
@@ -4,13 +4,17 @@ import I18n from 'react-native-i18n';
 import en from 'locales/en.json';
 import es from 'locales/es.json';
 import fr from 'locales/fr.json';
+import pt from 'locales/pt.json';
+import id from 'locales/id.json';
 
 I18n.fallbacks = true;
 
 I18n.translations = {
-  en: {...en},
-  es: {...es},
-  fr: {...fr}
+  en: { ...en },
+  es: { ...es },
+  fr: { ...fr },
+  pt: { ...pt },
+  id: { ...id }
 };
 
 export default I18n;
