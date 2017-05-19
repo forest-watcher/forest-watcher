ccc0babd69a12c3a3b651ea568288a10896945ec
j8seangel <jseangel@gmail.com>
2017-05-19 15:53:30 +0200

    Update helper to use dynamic initial dates

--- app/helpers/date.js
+++ app/helpers/date.js
@@ -1,7 +1,9 @@
 import moment from 'moment';
+import CONSTANTS from 'config/constants';
+
 // Receives a date in format: yyyyMMdd and returns the number of days since the start date
-export default function daysSince(date) {
-  const START_DATE = moment('20150101');
+export default function daysSince(date, start = CONSTANTS.startDate) {
+  const startDate = moment(start);
   const parsedDate = moment(date);
-  return String(parsedDate.diff(START_DATE, 'days'));
+  return String(parsedDate.diff(startDate, 'days'));
 }
