7c4beea773c5485378f21ace760be5225e127028
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-05-22 15:32:14 +0200

    Specify format in date constructor to avoid warning of fallback

--- app/helpers/date.js
+++ app/helpers/date.js
@@ -4,7 +4,7 @@ import CONSTANTS from 'config/constants';
 // Receives a date in format: YYYYMMDD and returns the number of days since the start date
 export function daysSince(date, start = CONSTANTS.startDate) {
   const startDate = moment(start);
-  const parsedDate = moment(date);
+  const parsedDate = moment(date, 'YYYYMMDD');
   return String(parsedDate.diff(startDate, 'days'));
 }
 
