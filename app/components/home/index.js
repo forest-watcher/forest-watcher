d2129924bbaceed73a9efaedf67212486c52dab7
sorodrigo <rodrigo.solis@vizzuality.com>
2017-06-02 11:31:38 +0200

    [wip] sync screen + sync texts + sync styles

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import {
   View,
-  ActivityIndicator
+  ActivityIndicator,
+  Text
 } from 'react-native';
+import ActionButton from 'components/common/action-button';
 
 import { getLanguage } from 'helpers/language';
 import Theme from 'config/theme';
+import I18n from 'locales';
 import tracker from 'helpers/googleAnalytics';
 import styles from './styles';
 
@@ -28,14 +31,17 @@ class Home extends Component {
     }
   }
 
+  // TODO: move to container
   isDifferentLanguage() {
     return this.props.language !== getLanguage();
   }
 
+  // TODO: transform into a single action => move to store
   cacheForms() {
-    if (!this.props.report || this.isDifferentLanguage()) this.props.getReportQuestions();
-    if (!this.props.dailyFeedback || this.isDifferentLanguage()) this.props.getFeedbackQuestions('daily');
-    if (!this.props.weeklyFeedback || this.isDifferentLanguage()) this.props.getFeedbackQuestions('weekly');
+    const isDifferentLanguage = this.isDifferentLanguage();
+    if (!this.props.report || isDifferentLanguage) this.props.getReportQuestions();
+    if (!this.props.dailyFeedback || isDifferentLanguage) this.props.getFeedbackQuestions('daily');
+    if (!this.props.weeklyFeedback || isDifferentLanguage) this.props.getFeedbackQuestions('weekly');
   }
 
   handleStatus() {
@@ -51,6 +57,7 @@ class Home extends Component {
         });
       }
 
+      // TODO: why does this escenario is possible?
       if (!this.props.user.hasData) {
         this.props.getUser();
       }
@@ -98,13 +105,64 @@ class Home extends Component {
   }
 
   render() {
+    const online = true;
+    const reach = 'wifi';
+    let title;
+    let subtitle;
+
+    if (online) {
+      title = reach === 'wifi' ? I18n.t('home.title.updating') : I18n.t('home.title.outOfDate');
+      subtitle = reach === 'wifi' ? I18n.t('home.subtitle.takesTime') : I18n.t('home.subtitle.mobile');
+    } else {
+      title = I18n.t('home.title.outOfDate');
+      subtitle = I18n.t('home.subtitle.noConnection');
+    }
+
     return (
       <View style={[styles.mainContainer, styles.center]}>
-        <ActivityIndicator
-          color={Theme.colors.color1}
-          style={{ height: 80 }}
-          size="large"
-        />
+        <View>
+          <View style={styles.textContainer}>
+            {online && reach === 'wifi' &&
+              <ActivityIndicator
+                color={Theme.colors.color1}
+                style={{ height: 80 }}
+                size="large"
+              />
+            }
+            <Text style={styles.title}>
+              {title}
+            </Text>
+            <Text style={styles.subtitle}>
+              {subtitle}
+            </Text>
+          </View>
+          {reach !== 'mobile' ?
+            <View>
+              <ActionButton
+                monochrome
+                noIcon
+                style={styles.button}
+                onPress={() => {}}
+                text={I18n.t('home.skip').toUpperCase()}
+              />
+            </View>
+            :
+            <View>
+              <ActionButton
+                monochrome
+                noIcon
+                onPress={() => {}}
+                text={I18n.t('home.skip').toUpperCase()}
+              />
+              <ActionButton
+                main
+                noIcon
+                onPress={() => {}}
+                text={I18n.t('home.update').toUpperCase()}
+              />
+            </View>
+          }
+        </View>
       </View>
     );
   }
--- app/components/home/styles.js
+++ app/components/home/styles.js
@@ -4,10 +4,27 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: Theme.background.main
+    backgroundColor: Theme.background.main,
+    flexDirection: 'column'
   },
   center: {
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-around'
+  },
+  textContainer: {
+    alignItems: 'center'
+  },
+  title: {
+    color: Theme.colors.color1,
+    fontSize: 21
+  },
+  subtitle: {
+    color: Theme.colors.color3,
+    marginTop: 18,
+    marginBottom: 25
+  },
+  button: {
+    left: 8,
+    right: 8,
+    position: 'absolute'
   }
 });
