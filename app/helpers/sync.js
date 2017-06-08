cf532dc3a941c7f0afba2863281d6fc072f04e4c
sorodrigo <rodrigo.solis@vizzuality.com>
2017-06-08 14:16:40 +0200

    Sum the areas data pending actions number

--- app/helpers/sync.js
+++ app/helpers/sync.js
@@ -12,7 +12,14 @@ export function getActionsPending(state) {
     !state.feedback.synced.daily && !state.feedback.syncing.daily,
     !state.feedback.synced.weekly && !state.feedback.syncing.weekly
   ];
-  return actionsPending.reduce((ac, next) => (next ? ac + 1 : ac), 0);
+  const actionsPendingCount = actionsPending.reduce((ac, next) => (next ? ac + 1 : ac), 0);
+
+  const { pendingData } = state.areas;
+  const areasDataPendingCount = Object.keys(pendingData).reduce((acc, next) => (
+    acc + Object.keys(pendingData[next]).length
+  ), 0);
+  // yes... I know
+  return actionsPendingCount + areasDataPendingCount;
 }
 
 export default { getReadyState, getActionsPending };
