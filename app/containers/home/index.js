c200def16cc632f0192f5cfbeafb2e26b524fb75
j8seangel <jseangel@gmail.com>
2017-06-14 20:03:55 +0200

    Total actions = actionsTodo + actionsInProgress

--- app/containers/home/index.js
+++ app/containers/home/index.js
@@ -2,13 +2,14 @@ import { connect } from 'react-redux';
 import { setLanguage, setSyncModal } from 'redux-modules/app';
 import { setLoginStatus } from 'redux-modules/user';
 import { getLanguage } from 'helpers/language';
-import { getReadyState } from 'helpers/sync';
+import { isSyncFinished } from 'helpers/sync';
+
 import Home from 'components/home';
 
 function mapStateToProps(state) {
   return {
     hasAreas: !!state.areas.data.length,
-    readyState: getReadyState(state),
+    syncFinished: isSyncFinished(state),
     loggedIn: state.user.loggedIn,
     token: state.user.token,
     languageChanged: state.app.language !== getLanguage(),
