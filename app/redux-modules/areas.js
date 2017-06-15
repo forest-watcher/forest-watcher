fefef011f67c9c9c39c0c199af72ceedc450b26e
sorodrigo <rodrigo.solis@vizzuality.com>
2017-06-15 12:28:04 +0200

    eslint fixes

--- app/redux-modules/areas.js
+++ app/redux-modules/areas.js
@@ -104,7 +104,7 @@ export default function reducer(state = initialState, action) {
         pendingData = {
           coverage: { ...pendingData.coverage, [newArea.id]: false },
           geostore: { ...pendingData.geostore, [newArea.id]: false },
-          image: { ...pendingData.image, [newArea.id]: false },
+          image: { ...pendingData.image, [newArea.id]: false }
         };
         if (newArea.cache) {
           pendingData.alert = { ...pendingData.alert, [newArea.id]: false };
