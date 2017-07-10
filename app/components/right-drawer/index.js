d74b76e9371fd7e9d4d1f993b55391415d4bdd5f
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-07-10 15:38:15 +0200

    feat: right drawer basic layout

--- app/components/right-drawer/index.js
+++ app/components/right-drawer/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  View
+  View,
+  Text
 } from 'react-native';
+import Row from 'components/common/row';
 import styles from './styles';
 
 class RightDrawer extends Component {
@@ -13,24 +15,49 @@ class RightDrawer extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      dummyValue: false
+    };
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
   }
 
   onNavigatorEvent(event) {
     switch (event.id) {
       case 'didAppear':
-        tron.log('Im here');
+        this.onDidAppear();
         break;
       case 'didDisappear':
-        tron.log('Im out');
+        this.onDidDisappear();
         break;
       default:
     }
   }
 
+  onDidAppear = () => {
+    tron.log('Im here');
+  }
+
+  onDidDisappear = () => {
+    tron.log('Im out');
+  }
+
   render() {
     return (
-      <View style={styles.mainContainer} />
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>Map settings</Text>
+        </View>
+        <View style={styles.body}>
+          <View style={styles.contextualLayersContainer}>
+            <Text>Contextual layers</Text>
+            <Row
+              text="Protected Areas"
+              value={this.state.dummyValue}
+              onValueChange={val => this.setState({ dummyValue: val })}
+            />
+          </View>
+        </View>
+      </View>
     );
   }
 }
--- app/components/right-drawer/styles.js
+++ app/components/right-drawer/styles.js
@@ -2,7 +2,7 @@ import Theme from 'config/theme';
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  mainContainer: {
+  container: {
     flex: 1,
     backgroundColor: Theme.background.main,
     flexDirection: 'column'
