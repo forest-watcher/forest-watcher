a966dce39b0bea9e4cb076a1b42cb97be8a0eaaf
j8seangel <jseangel@gmail.com>
2017-07-11 12:51:44 +0200

    Makes the cluster coordinates simpler

--- app/components/map/clusters/index.js
+++ app/components/map/clusters/index.js
@@ -10,37 +10,29 @@ const alertGlad = require('assets/alert-glad.png');
 const alertViirs = require('assets/alert-viirs.png');
 
 class Clusters extends PureComponent {
-
-  selectAlert = (index) => {
-    const marker = this.props.markers[index];
-    this.props.selectAlert({
-      latitude: marker.geometry.coordinates[1],
-      longitude: marker.geometry.coordinates[0]
-    });
-  }
-
   render() {
     if (!this.props.markers) return null;
     return (
       <View>
-        {this.props.markers.map((marker, index) => (
-          marker.properties.point_count !== undefined ? (
+        {this.props.markers.map((marker, index) => {
+          const coordinates = {
+            latitude: marker.geometry.coordinates[1],
+            longitude: marker.geometry.coordinates[0]
+          };
+          return marker.properties.point_count !== undefined ? (
             <ClusterMarker id={`cluster-marker-${index}`} key={`cluster-marker-${index}`} marker={marker} zoomTo={this.props.zoomTo} datasetSlug={this.props.datasetSlug} />
           ) : (
             <MapView.Marker
               key={`Alert-marker-${index}`}
-              coordinate={{
-                latitude: marker.geometry.coordinates[1],
-                longitude: marker.geometry.coordinates[0]
-              }}
+              coordinate={coordinates}
               image={this.props.datasetSlug === 'viirs' ? alertViirs : alertGlad}
-              onPress={() => this.selectAlert(index)}
+              onPress={() => this.props.selectAlert(coordinates)}
               zIndex={1}
               draggable={false}
               anchor={{ x: 0.5, y: 0.5 }}
             />
-          )
-        ))}
+          );
+        })}
       </View>
     );
   }
