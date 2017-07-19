ac519e7ae6656f3ca75ca67babdbdfa0bb88e753
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-07-19 15:35:37 +0200

    feat: invalidates redux-persist on version change

--- app/offline/index.js
+++ app/offline/index.js
@@ -3,11 +3,20 @@ import offlineConfig from 'redux-offline/lib/defaults';
 import detectNetwork from 'redux-offline/lib/defaults/detectNetwork.native';
 import { AsyncStorage } from 'react-native'; // eslint-disable-line import/no-unresolved
 import { persistStore } from 'redux-persist';
+import { version } from 'package.json';
 import effect from './effect';
 
-const persistNative = (store, options, callback) => (
-  persistStore(store, { storage: AsyncStorage, ...options }, callback) // .purge to clean the offline data
-);
+const persistNative = (store, options, callback) => {
+  AsyncStorage.getItem('reduxPersist:app')
+    .then((app) => {
+      const getPersistedStore = () => persistStore(store, { storage: AsyncStorage, ...options }, callback); // .purge to clean the offline data
+      if (app.version !== version) {
+        getPersistedStore().purge();
+      } else {
+        getPersistedStore();
+      }
+    });
+};
 
 const config = params => ({
   ...offlineConfig,
