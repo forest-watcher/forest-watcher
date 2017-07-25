357b7a3edede58d0ad1ffa17996d096998b83fe2
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-07-25 20:42:08 +0200

    feat: dashboard redesign

--- app/components/dashboard/styles.js
+++ app/components/dashboard/styles.js
@@ -14,58 +14,22 @@ export default StyleSheet.create({
     flex: 1,
     backgroundColor: Theme.background.main
   },
+  label: {
+    marginLeft: 16,
+    fontSize: 17,
+    color: Theme.fontColors.light,
+    fontFamily: Theme.font,
+    fontWeight: '400'
+  },
   list: {
     flex: 1
   },
-  content: {
-    flexWrap: 'wrap',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    paddingTop: 18
+  listContent: {
+    paddingTop: 10,
+    paddingBottom: 30
   },
-  item: {
-    width: (Theme.screen.width / 2) - 12,
-    height: 184,
-    marginLeft: 8,
-    marginBottom: 22,
-    backgroundColor: Theme.background.white,
-    borderBottomColor: Theme.borderColors.main,
-    borderBottomWidth: 2
-  },
-  imageIcon: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 16
-  },
-  buttonRound: {
-    width: Theme.screen.width - 24,
-    backgroundColor: Theme.colors.color3,
-    height: 48,
-    marginBottom: 8,
-    borderRadius: 32,
-    justifyContent: 'center',
-    overflow: 'hidden',
-    marginLeft: 12,
-    marginTop: 8
-  },
-  buttonTextRound: {
-    color: Theme.background.white,
-    marginLeft: 24
-  },
-  icon: {
-    width: 80,
-    height: 60,
-    borderWidth: 0.5,
-    borderColor: '#333'
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '400',
-    marginTop: 15,
-    marginBottom: 5,
-    fontFamily: Theme.font,
-    color: Theme.fontColors.secondary
+  row: {
+    marginTop: 32
   },
   iconSettings: {
     position: 'absolute',
