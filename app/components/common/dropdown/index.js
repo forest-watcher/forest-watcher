8232a72120ad61cd51ddce5b2252012a29c51312
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-08-21 12:40:26 +0200

    feat: refactors picker into generic dropdown

---
+++ app/components/common/dropdown/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  Text,
+  Picker
+} from 'react-native';
+
+import I18n from 'locales';
+import styles from './styles';
+
+const Dropdown = (props) => {
+  const { label, selectedValue, options, onValueChange } = props;
+  return (
+    <View style={styles.coordinatesSection}>
+      <Text style={styles.containerLabel}>
+        {I18n.t(label)}
+      </Text>
+      <View style={styles.container}>
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={selectedValue}
+            onValueChange={onValueChange}
+            itemStyle={{ height: 72 }} // Only for iOS platform
+          >
+            {options.map(option => (<Picker.Item {...option} />))}
+          </Picker>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+Dropdown.propTypes = {
+  label: PropTypes.string.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+  onValueChange: PropTypes.func.isRequired,
+  options: PropTypes.shape({
+    key: PropTypes.string,
+    label: PropTypes.string,
+    value: PropTypes.string
+  }).isRequired
+};
+
+export default Dropdown;
