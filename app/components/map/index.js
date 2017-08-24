d5c1f871686f5a310a30b3c91413bc2f11ecd3dc
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-08-24 12:44:36 +0200

    fix: removes mapKey & region & initialRegion props from map

--- app/components/map/index.js
+++ app/components/map/index.js
@@ -208,7 +208,6 @@ class Map extends Component {
   componentWillUnmount() {
     Location.stopUpdatingLocation();
     Timer.clearTimeout(this, 'setAlerts');
-    Timer.cancelAnimationFrame(this, 'updateAreaFitBounds');
     if (this.eventLocation) {
       this.eventLocation.remove();
     }
@@ -315,13 +314,6 @@ class Map extends Component {
     });
   }
 
-  getMapKey = () => {
-    const { markers } = this.state;
-    const { contextualLayer } = this.props;
-    const layer = contextualLayer ? contextualLayer.name : '';
-    return `map-key-${markers.length}-${layer}`;
-  }
-
   updateMarkers(clean = false) {
     const { region } = this.state;
     const clusters = this.props.clusters && this.props.clusters.getClusters([
@@ -404,7 +396,7 @@ class Map extends Component {
     this.animateGeo();
 
     navigator.geolocation.getCurrentPosition(
-      (location) => {
+      throttle((location) => {
         const coords = typeof location.coords !== 'undefined' ? location.coords : location;
         this.setState({
           lastPosition: {
@@ -412,7 +404,7 @@ class Map extends Component {
             longitude: coords.longitude
           }
         });
-      },
+      }, 300),
       (error) => console.info(error),
       { enableHighAccuracy: true, timeout: 30000, maximumAge: 0 }
     );
@@ -553,7 +545,7 @@ class Map extends Component {
       const margin = Platform.OS === 'ios' ? 150 : 250;
       const options = { edgePadding: { top: margin, right: margin, bottom: margin, left: margin }, animated: false };
       if (this.map) {
-        Timer.requestAnimationFrame(this, 'updateAreaFitBounds', () => this.map.fitToCoordinates(this.props.areaCoordinates, options));
+        this.map.fitToCoordinates(this.props.areaCoordinates, options);
       }
     });
   }
@@ -633,8 +625,7 @@ class Map extends Component {
     let veilHeight = 120;
     if (hasAlertsSelected) veilHeight = hasNeighbours ? 260 : 180;
     const isIOS = Platform.OS === 'ios';
-    const mapKey = isIOS ? this.getMapKey() : 'mapView';
-    const mapRegion = isIOS ? this.state.region : undefined;
+
     // Map elements
     const basemapLayerElement = isConnected // eslint-disable-line
       ? (
@@ -658,14 +649,14 @@ class Map extends Component {
       ? isConnected
         ? (
           <MapView.UrlTile
-            key="contextualLayerElement"
+            key={`contextualLayerElement-${contextualLayer.name}`}
             urlTemplate={contextualLayer.url}
             zIndex={1}
           />
         )
         : (
           <MapView.LocalTile
-            key="contextualLayerElementOffline"
+            key={`contextualLayerElementOffline-${contextualLayer.name}`}
             localTemplate={ctxLayerLocalTilePath}
             zIndex={1}
           />
@@ -748,7 +739,7 @@ class Map extends Component {
       : null;
     const clustersElement = datasetSlug ? (
       <Clusters
-        key="clustersElement"
+        key={`clusters-${markers.length}`}
         markers={markers}
         selectAlert={this.selectAlert}
         zoomTo={this.zoomTo}
@@ -765,7 +756,6 @@ class Map extends Component {
           />
         </View>
         <MapView
-          key={mapKey}
           ref={(ref) => { this.map = ref; }}
           style={styles.map}
           provider={MapView.PROVIDER_GOOGLE}
@@ -773,8 +763,6 @@ class Map extends Component {
           minZoomLevel={2}
           maxZoomLevel={18}
           rotateEnabled={false}
-          initialRegion={mapRegion}
-          region={mapRegion}
           onRegionChangeComplete={this.updateRegion}
           onLayout={this.onLayout}
           moveOnMarkerPress={false}
