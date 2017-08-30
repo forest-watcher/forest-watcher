ef37cd284b6d1c500bf201a62d6e86a035d4c971
Hector <hi@hectorarce.es>
2017-08-30 10:55:39 +0200

    improve views

--- app/components/settings/faq/detail/index.js
+++ app/components/settings/faq/detail/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Hyperlink from 'react-native-hyperlink';
 import PropTypes from 'prop-types';
 import {
@@ -11,91 +11,83 @@ import {
 import Theme from 'config/theme';
 import styles from './styles';
 
-class FaqDetail extends Component {
-  static navigatorStyle = {
-    navBarTextColor: Theme.colors.color1,
-    navBarButtonColor: Theme.colors.color1,
-    topBarElevationShadowEnabled: false,
-    navBarBackgroundColor: Theme.background.main
-  };
+function FaqDetail(props) {
+  const { description, orderList, orderListLetters, footerText } = props.contentFaq;
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.containerContent}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+    >
 
-  static propTypes = {
-    contentFaq: PropTypes.any
-  };
-
-  state = {
-    description: this.props.contentFaq.description,
-    orderList: this.props.contentFaq.orderList,
-    orderListLetters: this.props.contentFaq.orderListLetters,
-    footerText: this.props.contentFaq.footerText
-  };
-
-  render() {
-    return (
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.containerContent}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
+      <View
+        style={styles.faq}
       >
 
-        <View
-          style={styles.faq}
-        >
+        {description && description.map((data, key) => (
+          <Hyperlink
+            key={key}
+            linkDefault linkStyle={{ color: '#97be32' }}
+            linkText={(url) => (url === 'mailto:forestwatcher@wri.org' ? 'forestwatcher@wri.org' : url)}
+          >
+            <Text style={styles.faqText}>{data.text}</Text>
+          </Hyperlink>
+        ))}
 
-          {this.state.description && this.state.description.map((data, key) => (
-            <Hyperlink
-              key={key}
-              linkDefault linkStyle={{ color: '#97be32' }}
-              linkText={(url) => (url === 'mailto:forestwatcher@wri.org' ? 'forestwatcher@wri.org' : url)}
-            >
+        {orderList && orderList.map((data, key) => (
+          <View
+            key={key}
+            style={description === '' ? styles.faqListNoPadding : styles.faqList}
+          >
+            <View style={styles.faqDotList}>{}</View>
+            <Hyperlink linkDefault linkStyle={{ color: '#97be32' }}>
               <Text style={styles.faqText}>{data.text}</Text>
             </Hyperlink>
-          ))}
+          </View>
+        ))}
 
-          {this.state.orderList && this.state.orderList.map((data, key) => (
-            <View
-              key={key}
-              style={this.state.description === '' ? styles.faqListNoPadding : styles.faqList}
-            >
-              <View style={styles.faqDotList}>{}</View>
+        {orderListLetters && orderListLetters.map((data, key) => (
+          <View key={key}>
+            <View style={styles.faqListLetter}>
+              <Text style={styles.faqText}>{String.fromCharCode(97 + (key % 27))}. </Text>
               <Hyperlink linkDefault linkStyle={{ color: '#97be32' }}>
                 <Text style={styles.faqText}>{data.text}</Text>
               </Hyperlink>
             </View>
-          ))}
-
-          {this.state.orderListLetters && this.state.orderListLetters.map((data, key) => (
-            <View key={key}>
-              <View style={styles.faqListLetter}>
-                <Text style={styles.faqText}>{String.fromCharCode(97 + (key % 27))}. </Text>
-                <Hyperlink linkDefault linkStyle={{ color: '#97be32' }}>
-                  <Text style={styles.faqText}>{data.text}</Text>
-                </Hyperlink>
-              </View>
-              <View style={styles.faqListLetterDescription}>
-                <Text style={styles.faqText}>i. </Text>
-                <Hyperlink
-                  linkDefault
-                  linkStyle={{ color: '#97be32' }}
-                  linkText={(url) => (url === 'mailto:forestwatcher@wri.org' ? 'forestwatcher@wri.org' : url)}
-                >
-                  <Text style={styles.faqText}>{data.description}</Text>
-                </Hyperlink>
-              </View>
+            <View style={styles.faqListLetterDescription}>
+              <Text style={styles.faqText}>i. </Text>
+              <Hyperlink
+                linkDefault
+                linkStyle={{ color: '#97be32' }}
+                linkText={(url) => (url === 'mailto:forestwatcher@wri.org' ? 'forestwatcher@wri.org' : url)}
+              >
+                <Text style={styles.faqText}>{data.description}</Text>
+              </Hyperlink>
             </View>
-          ))}
+          </View>
+        ))}
 
-          {this.state.footerText && this.state.footerText.map((data, key) => (
-            <Hyperlink key={key} linkDefault linkStyle={{ color: '#97be32' }}>
-              <Text style={styles.faqText}>{data.text}</Text>
-            </Hyperlink>
-          ))}
-        </View>
+        {footerText && footerText.map((data, key) => (
+          <Hyperlink key={key} linkDefault linkStyle={{ color: '#97be32' }}>
+            <Text style={styles.faqText}>{data.text}</Text>
+          </Hyperlink>
+        ))}
+      </View>
 
-      </ScrollView>
-    );
-  }
+    </ScrollView>
+  );
 }
+
+FaqDetail.navigatorStyle = {
+  navBarTextColor: Theme.colors.color1,
+  navBarButtonColor: Theme.colors.color1,
+  topBarElevationShadowEnabled: false,
+  navBarBackgroundColor: Theme.background.main
+};
+
+FaqDetail.propTypes = {
+  contentFaq: PropTypes.any
+};
 
 export default FaqDetail;
--- app/components/settings/terms-and-conditions/detail/index.js
+++ app/components/settings/terms-and-conditions/detail/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Hyperlink from 'react-native-hyperlink';
 import PropTypes from 'prop-types';
 import {
@@ -11,57 +11,51 @@ import {
 import Theme from 'config/theme';
 import styles from './styles';
 
-class TermsAndConditionsDetail extends Component {
-  static navigatorStyle = {
-    navBarTextColor: Theme.colors.color1,
-    navBarButtonColor: Theme.colors.color1,
-    topBarElevationShadowEnabled: false,
-    navBarBackgroundColor: Theme.background.main
-  };
+function TermsAndConditionsDetail(props) {
+  const { description, list } = props.contentTerm;
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.containerContent}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+    >
 
-  static propTypes = {
-    contentTerm: PropTypes.any
-  };
-
-  state = {
-    description: this.props.contentTerm.description,
-    list: this.props.contentTerm.list
-  };
-
-  render() {
-    return (
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.containerContent}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
+      <View
+        style={styles.terms}
       >
 
-        <View
-          style={styles.terms}
+        {description && <Hyperlink
+          linkDefault linkStyle={{ color: '#97be32' }}
+          linkText={(url) => (url === 'mailto:gfw@wri.org' ? 'gfw@wri.org' : url)}
         >
+          <Text style={styles.termsText}>{description}</Text>
+        </Hyperlink>}
 
-          {this.state.description && <Hyperlink
-            linkDefault linkStyle={{ color: '#97be32' }}
-            linkText={(url) => (url === 'mailto:gfw@wri.org' ? 'gfw@wri.org' : url)}
+        {list && list.map((data, key) => (
+          <View
+            key={key}
+            style={description ? styles.termsListNoPadding : styles.termsList}
           >
-            <Text style={styles.termsText}>{this.state.description}</Text>
-          </Hyperlink>}
+            <Text style={styles.termsText}>{String.fromCharCode(97 + (key % 27))}. </Text>
+            <Text style={styles.termsText}>{data.text}</Text>
+          </View>
+        ))}
+      </View>
 
-          {this.state.list && this.state.list.map((data, key) => (
-            <View
-              key={key}
-              style={this.state.description ? styles.termsListNoPadding : styles.termsList}
-            >
-              <Text style={styles.termsText}>{String.fromCharCode(97 + (key % 27))}. </Text>
-              <Text style={styles.termsText}>{data.text}</Text>
-            </View>
-          ))}
-        </View>
-
-      </ScrollView>
-    );
-  }
+    </ScrollView>
+  );
 }
+
+TermsAndConditionsDetail.navigatorStyle = {
+  navBarTextColor: Theme.colors.color1,
+  navBarButtonColor: Theme.colors.color1,
+  topBarElevationShadowEnabled: false,
+  navBarBackgroundColor: Theme.background.main
+};
+
+TermsAndConditionsDetail.propTypes = {
+  contentTerm: PropTypes.object.isRequired
+};
 
 export default TermsAndConditionsDetail;
