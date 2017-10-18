f26cb733409a32d29351018fbbbcf532cf023733
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-10-18 09:12:56 +0200

    fix: android dependencies import breaking changes

--- android/app/src/main/java/com/forestwatcher/MainApplication.java
+++ android/app/src/main/java/com/forestwatcher/MainApplication.java
@@ -1,10 +1,5 @@
 package com.forestwatcher;
 
-import android.app.Application;
-import android.util.Log;
-import android.support.annotation.NonNull;
-
-import com.facebook.react.ReactApplication;
 import com.psykar.cookiemanager.CookieManagerPackage;
 import io.realm.react.RealmReactPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
@@ -12,14 +7,10 @@ import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.sensormanager.SensorManagerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 import com.airbnb.android.react.maps.MapsPackage;
-import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.syarul.rnlocation.RNLocation;
 import com.rnziparchive.RNZipArchivePackage;
 import com.imagepicker.ImagePickerPackage;
@@ -27,49 +18,35 @@ import com.imagepicker.ImagePickerPackage;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+public class MainApplication extends NavigationApplication {
 
   @Override
   public boolean isDebug() {
-      return BuildConfig.DEBUG;
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
   }
 
-  @NonNull
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+            new RNGoogleSigninPackage(),
+            new GoogleAnalyticsBridgePackage(),
+            new SensorManagerPackage(),
+            new RNFetchBlobPackage(),
+            new ReactNativeConfigPackage(),
+            new MapsPackage(),
+            new RNI18nPackage(),
+            new RNLocation(),
+            new RealmReactPackage(),
+            new RNZipArchivePackage(),
+            new CookieManagerPackage(),
+            new ImagePickerPackage()
+    );
+  }
+
   @Override
   public List<ReactPackage> createAdditionalReactPackages() {
-    // Add additional packages you require here
-    return Arrays.<ReactPackage>asList(
-      new RNGoogleSigninPackage(),
-      new GoogleAnalyticsBridgePackage(),
-      new SensorManagerPackage(),
-      new RNFetchBlobPackage(),
-      new ReactNativeConfigPackage(),
-      new MapsPackage(),
-      new ReactNativeI18n(),
-      new RNLocation(),
-      new RealmReactPackage(),
-      new RNZipArchivePackage(),
-      new CookieManagerPackage(),
-      new ImagePickerPackage()
-    );
+    return getPackages();
   }
 }
