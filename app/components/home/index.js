73979bb828f12014439237c538034b59ea30a3b1
j8seangel <jseangel@gmail.com>
2018-06-15 12:30:52 +0200

    Set app language on rehydrate action

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -14,7 +14,6 @@ type Props = {
   loggedIn: boolean,
   token: string,
   isAppSynced: boolean,
-  setLanguage: () => void,
   navigator: Object,
   hasAreas: boolean,
   actionsPending: number,
@@ -40,21 +39,8 @@ class Home extends Component<Props> {
     this.handleStatus();
   }
 
-  // Override shouldComponentUpdate because setLanguage passed as prop always changes
-  shouldComponentUpdate(nextProps: Props) {
-    const conditions = [
-      this.props.loggedIn !== nextProps.loggedIn,
-      this.props.isAppSynced !== nextProps.isAppSynced,
-      this.props.hasAreas !== nextProps.hasAreas,
-      this.props.token !== nextProps.token,
-      this.props.actionsPending !== nextProps.actionsPending
-    ];
-    return conditions.includes(true);
-  }
-
   componentDidUpdate() {
     this.handleStatus();
-    this.props.setLanguage();
   }
 
   handleStatus() {
--- app/containers/home/index.js
+++ app/containers/home/index.js
@@ -3,8 +3,7 @@ import type { State } from 'types/store.types';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { syncApp, setLanguage, setAppSynced } from 'redux-modules/app';
-import { getLanguage } from 'helpers/language';
+import { syncApp, setAppSynced } from 'redux-modules/app';
 import { getTotalActionsPending } from 'helpers/sync';
 
 import Home from 'components/home';
@@ -15,28 +14,16 @@ function mapStateToProps(state: State) {
     isAppSynced: state.app.synced,
     loggedIn: state.user.loggedIn,
     token: state.user.token,
-    actionsPending: getTotalActionsPending(state),
-    languageChanged: state.app.language !== getLanguage()
+    actionsPending: getTotalActionsPending(state)
   };
 }
 
 const mapDispatchToProps = (dispatch: *) => bindActionCreators({
   syncApp,
-  setAppSynced,
-  setLanguageDispatch: () => setLanguage(getLanguage())
+  setAppSynced
 }, dispatch);
-
-function mergeProps({ languageChanged, ...stateProps }, { setLanguageDispatch, ...dispatchProps }, ownProps) {
-  return {
-    ...ownProps,
-    ...dispatchProps,
-    ...stateProps,
-    setLanguage: () => (languageChanged && setLanguageDispatch())
-  };
-}
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-  mergeProps
+  mapDispatchToProps
 )(Home);
