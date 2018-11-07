4af03beee7259b82be5f4143845151395323cdc1
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-07 15:11:16 +0000

    Added Jest unit tests, mocks and snapshots.

---
+++ __mocks__/Dimensions.js
@@ -0,0 +1,6 @@
+
+const Dimensions = {
+  get: jest.fn().mockReturnValue({width: 100, height: 100}),
+};
+
+module.exports = Dimensions;
--- __tests__/index.js
+++ __tests__/index.js
@@ -1,10 +1,32 @@
 import 'react-native';
 import React from 'react';
-// import Index from '../index.js';
+import { setOfflineMode } from '../app/redux-modules/app';
 
-// Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
+describe('Unit Tests', () => {
 
-it('renders correctly', () => {
-  expect(true);
+  it('test runs', () => {
+    expect(true);
+  });
+
+  it('snapshot runs', () => {
+    expect(false).toMatchSnapshot();
+  });
+
+  it('snapshot object test', () => {
+    expect({'hello': true, number: 232}).toMatchSnapshot();
+  });
+
+  it('snapshot redux action test', () => {
+    expect(setOfflineMode(true)).toMatchSnapshot();
+    expect(setOfflineMode(false)).toMatchSnapshot();
+  });
+
 });
+
+// MOCKS
+
+jest.mock('react-native-config', () => {return {API_URL: '', MAPBOX_TOKEN: ''}});
+jest.mock('react-native-cookies', () => {clearAll: jest.fn()});
+jest.mock('react-native-fetch-blob', () => {config: jest.fn()});
+jest.mock('react-native-zip-archive', () => {unzip: jest.fn()});
+jest.mock('Dimensions');
