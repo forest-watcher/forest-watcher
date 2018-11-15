51959edced1a6ee1d8facf2f426dbdd624a6ff96
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-15 17:53:39 +0000

    Added snapshot test for entire redux store initial state.

---
+++ __tests__/combinedReducerTest.js
@@ -0,0 +1,26 @@
+import 'react-native';
+import { combinedReducer } from 'combinedReducer';
+import { retrySync } from 'redux-modules/app';
+
+// TEST
+
+/**
+ * We're testing the actions and reducers independently.
+ * This let's us test the whole store's state, but at a much lower level
+ * and with better insights into how the actions are called/dispatched.
+ * todo: explain -> component-like testing
+ */
+describe('async actions', () => {
+  it('combined Reducer test', () => {
+    // todo: use Any<Number> for syncDate field in snapshot
+    expect(combinedReducer(undefined, { type: 'unknownActionTest' })).toMatchSnapshot();
+  });
+});
+
+// MOCKS
+/* eslint-disable */
+jest.mock('react-native-config', () => ({ API_URL: '', MAPBOX_TOKEN: '' }));
+jest.mock('react-native-cookies', () => { clearAll: jest.fn(); });
+jest.mock('react-native-fetch-blob', () => { config: jest.fn(); });
+jest.mock('react-native-zip-archive', () => { unzip: jest.fn(); });
+jest.mock('Dimensions');
