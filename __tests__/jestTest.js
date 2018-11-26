f01524cbe3bb56659351be2bda07abb12dc9c6bc
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-26 17:07:16 +0000

    Updated redux unit tests.

--- __tests__/jestTest.js
+++ __tests__/jestTest.js
@@ -1,7 +1,7 @@
 import 'react-native';
-import 'jest';
-import appReducer, { setOfflineMode, syncApp } from '../app/redux-modules/app';
+import appReducer, { setAppSynced, setOfflineMode, syncApp } from 'redux-modules/app';
 
+// JEST TESTS
 describe('Unit Tests', () => {
   it('test runs', () => {
     expect(true);
@@ -10,18 +10,35 @@ describe('Unit Tests', () => {
   it('snapshot runs', () => {
     expect({ hello: true, number: 232 }).toMatchSnapshot();
   });
+});
 
+// ACTION TESTS
+describe('Redux Actions - Unit Tests', () => {
   it('snapshot redux action test', () => {
     expect(setOfflineMode(true)).toMatchSnapshot();
     expect(setOfflineMode(false)).toMatchSnapshot();
   });
 
-  it('snapshot redux reducer test', () => {
-    expect(appReducer(undefined, setOfflineMode(true))).toMatchSnapshot();
+  it('snapshot redux action thunk test', () => {
+    expect(syncApp()).toMatchSnapshot(); // thunked action tests don't work currently - no useful output
+  });
+});
+
+// REDUCER & STATE TESTS
+describe('Redux Reducers - Unit Tests', () => {
+  it('snapshot redux reducer initial state test', () => {
+    expect(appReducer(undefined, { type: 'NONE' })).toMatchSnapshot();
   });
 
-  it('snapshot sync app function test', () => {
-    expect(syncApp()).toMatchSnapshot();
+  it('snapshot redux reducer action test', () => {
+    expect(appReducer(undefined, setAppSynced(true))).toMatchSnapshot();
+  });
+
+  it('snapshot redux reducer multiple action test', () => {
+    let state = appReducer(undefined, setAppSynced(true));
+    expect(state).toMatchSnapshot();
+    state = appReducer(state, setOfflineMode(true));
+    expect(state).toMatchSnapshot();
   });
 });
 
