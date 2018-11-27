5a9996060ee7b6e8105bb8dc76bf743c57d14cc9
Michael P-F <mikeyredtiger22@gmail.com>
2018-11-27 15:54:42 +0000

    Mock async / thunked redux actions and snapshot test actions and state.

--- __tests__/redux-tests/appModuleTest.js
+++ __tests__/redux-tests/appModuleTest.js
@@ -62,14 +62,22 @@ describe('Redux App Module', () => {
       store = configuredStore(initialStoreState);
     });
 
+    function mockDispatchAction(state, action) {
+      const newStore = configuredStore({ app: state });
+      newStore.dispatch(action);
+      const resolvedActions = newStore.getActions();
+      expect(resolvedActions).toMatchSnapshot();
+      let newState = state;
+      // should only be one but the loop is used for future changes and so all tests conform.
+      resolvedActions.forEach(resolvedAction => {
+        newState = appReducer(newState, resolvedAction); // todo should pass reducer in?
+      }); // todo: how one thunk calling another is handled
+      expect(newState).toMatchSnapshot();
+      return newState;
+    }
+
     it('retrySync', () => {
       store.dispatch(retrySync());
-      expect(store.getActions()).toMatchSnapshot();
-    });
-
-    it('showNotConnectedNotification', () => {
-      store.dispatch(showNotConnectedNotification());
-      // todo find way to call setOfflineMode(true) then dispatch showNotConnectedNotification again
       expect(store.getActions()).toMatchSnapshot();
     });
 
@@ -77,13 +85,35 @@ describe('Redux App Module', () => {
       store.dispatch(syncApp());
       expect(store.getActions()).toMatchSnapshot();
     });
+
+    it('showNotConnectedNotification', () => {
+      store.dispatch(showNotConnectedNotification());
+      expect(store.getActions()).toMatchSnapshot();
+    });
+
+    // it('syncApp full test', () => {
+    //   store.dispatch(syncApp());
+    //   // todo test with user.loggedIn == true
+    //   expect(store.getActions()).toMatchSnapshot();
+    // });
+
+    it('showNotConnectedNotification full test', () => {
+      // Although this is super cool, changing the steps (actions) will make
+      // the snapshot diff very unreadable (but still very helpful).
+      let newState = appReducer(undefined, { type: 'NONE' }); // only models the app module state object
+      // todo: can for loop actions:
+      newState = mockDispatchAction(newState, showNotConnectedNotification());
+      newState = mockDispatchAction(newState, setOfflineMode(true));
+      /* newState = */
+      mockDispatchAction(newState, showNotConnectedNotification());
+    });
   });
 
 // REDUCER & STATE TESTS
-  describe('Redux Reducers - Unit Tests', () => {
-    it('tests', () => {
-      expect(true);
-    });
+  // describe('Redux Reducers - Unit Tests', () => {
+    // it('tests', () => {
+    //   expect(true);
+    // });
     // it('snapshot redux reducer initial state test', () => {
     //   expect(appReducer(undefined, { type: 'NONE' })).toMatchSnapshot();
     // });
@@ -98,7 +128,7 @@ describe('Redux App Module', () => {
     //   state = appReducer(state, setOfflineMode(true));
     //   expect(state).toMatchSnapshot();
     // });
-  });
+  // });
 });
 
 // MOCKS
