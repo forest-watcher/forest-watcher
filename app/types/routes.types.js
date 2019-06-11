a946482d124533cacad25108068823b1ae8a0698
Tim Matthews <tim@3sidedcube.com>
2019-06-11 16:33:49 +0100

    Added missing route types for new action

--- app/types/routes.types.js
+++ app/types/routes.types.js
@@ -34,9 +34,15 @@ export type RouteDeletionCriteria = {
   areaId: string
 };
 
-export type RouteAction = finishAndSaveRoute | deleteRouteAction | updateActiveRoute | discardActiveRoute;
+export type RouteAction =
+  | finishAndSaveRoute
+  | deleteRouteAction
+  | updateActiveRoute
+  | updateSavedRoute
+  | discardActiveRoute;
 
 type updateActiveRoute = { type: 'routes/UPDATE_ACTIVE_ROUTE', payload: Route };
+type updateSavedRoute = { type: 'routes/UPDATE_SAVED_ROUTE', payload: Route };
 type finishAndSaveRoute = { type: 'routes/FINISH_AND_SAVE_ROUTE' };
 type deleteRouteAction = { type: 'routes/DELETE_ROUTE', payload: RouteDeletionCriteria };
 type discardActiveRoute = { type: 'routes/DISCARD_ACTIVE_ROUTE' };
