72ec6b34f35d5e19965dcfff8db25a737bb7308c
Tim Matthews <tim@3sidedcube.com>
2019-06-14 15:53:24 +0100

    Fixed MultiPolygon geojson areas not being handled correctly by the app causing crash in (FOREST-WATCHER-MOBILE-CLIENT-2W)

--- app/containers/map/index.js
+++ app/containers/map/index.js
@@ -14,10 +14,24 @@ import { getSelectedArea, activeDataset } from 'helpers/area';
 import Map from 'components/map';
 
 function getAreaCoordinates(areaFeature) {
-  return areaFeature.geometry.coordinates[0].map(coordinate => ({
-    longitude: coordinate[0],
-    latitude: coordinate[1]
-  }));
+  switch (areaFeature.geometry.type) {
+    case 'MultiPolygon': {
+      // When KML files are uploaded in the webapp they are always turned into MultiPolygons even if that multi polygon
+      // only consists of a single polygon - just take the first polygon
+      return areaFeature.geometry.coordinates[0][0].map(coordinate => ({
+        longitude: coordinate[0],
+        latitude: coordinate[1]
+      }));
+    }
+    case 'Polygon':
+    default: {
+      // Handle anything we don't recognise as a Polygon
+      return areaFeature.geometry.coordinates[0].map(coordinate => ({
+        longitude: coordinate[0],
+        latitude: coordinate[1]
+      }));
+    }
+  }
 }
 
 function reconcileRoutes(activeRoute, previousRoute) {
