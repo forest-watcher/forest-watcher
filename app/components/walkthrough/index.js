06497d8b755a7eb873830db23d75d58a191a18f4
Ryan Bourne <ryan.bourne@3sidedcube.com>
2019-08-09 11:24:17 +0100

    GFW-352 - re-added empty view to allow swiping to the login screen

--- app/components/walkthrough/index.js
+++ app/components/walkthrough/index.js
@@ -173,6 +173,7 @@ class Walkthrough extends PureComponent {
                 )}
               </View>
             ))}
+            <View>{/* This view is required to force the slider to navigate to login on the last slide */}</View>
           </StepsSlider>
           <View style={[styles.footer, { justifyContent: footerJustifyContent, marginBottom: bottomSafeAreaInset }]}>
             {page > 0 && ( // Buttons are placed here because inside the StepsSlider the events wont trigger
