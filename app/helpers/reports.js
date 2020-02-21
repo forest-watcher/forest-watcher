9ec21f5cc1b4c2339ea8d05ea5f61445aeed59a6
Simon Mitchell <simon@3sidedcube.com>
2020-02-21 11:45:20 +0000

    Fixes some formatting

--- app/helpers/reports.js
+++ app/helpers/reports.js
@@ -8,20 +8,18 @@ const reportNameRegex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/g;
  * @return {string} A localised, readable version of the string
  */
 export function readableNameForReportName(reportName: string) {
-	// Using a full regex match for the end of the report name here including data so if user names there area something like SAMS-WIGGLY-REPORT 
-	// it doesn't intefere with our logic
-	const result = reportNameRegex.exec(reportName);
-	if (!result || result.length < 2) {
-		return reportName;
-	}
-	switch (result[1]) {
-		case "VIIRS":
-			return i18n.t('report.viirs');
-		break;
-		case "GLAD":
-			return i18n.t('report.glad');
-		break;
-		default:
-		return reportName;
-	}
+  // Using a full regex match for the end of the report name here including data so if user names there area something like SAMS-WIGGLY-REPORT
+  // it doesn't intefere with our logic
+  const result = reportNameRegex.exec(reportName);
+  if (!result || result.length < 2) {
+    return reportName;
+  }
+  switch (result[1]) {
+    case 'VIIRS':
+      return i18n.t('report.viirs');
+    case 'GLAD':
+      return i18n.t('report.glad');
+    default:
+      return reportName;
+  }
 }
