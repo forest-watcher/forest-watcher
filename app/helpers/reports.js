09d41defb9ec08ad08b644df795f2d14b2bd8693
Simon Mitchell <simon@3sidedcube.com>
2020-02-21 11:39:16 +0000

    Makes more requested PR changes

--- app/helpers/reports.js
+++ app/helpers/reports.js
@@ -1,5 +1,7 @@
 import i18n from 'locales';
 
+const reportNameRegex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/g;
+
 /**
  * Converts a report name, to a readable, localised string
  * @param {string} reportName - The report name to convert
@@ -8,9 +10,8 @@ import i18n from 'locales';
 export function readableNameForReportName(reportName: string) {
 	// Using a full regex match for the end of the report name here including data so if user names there area something like SAMS-WIGGLY-REPORT 
 	// it doesn't intefere with our logic
-	const regex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/g;
-	const result = regex.exec(reportName);
-	if (!result) {
+	const result = reportNameRegex.exec(reportName);
+	if (!result || result.length < 2) {
 		return reportName;
 	}
 	switch (result[1]) {
