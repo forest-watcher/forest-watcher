7145903c50a3c82ac23e4b6c160bed2baffe1002
Michael P-F <michael@3sidedcube.com>
2020-02-25 09:47:30 +0000

    Remove old map alert functionality.

--- app/components/map/index.js
+++ app/components/map/index.js
@@ -6,32 +6,25 @@ import { Sentry } from 'react-native-sentry';
 
 import { REPORTS } from 'config/constants';
 import throttle from 'lodash/throttle';
-import debounce from 'lodash/debounce';
 import isEqual from 'lodash/isEqual';
 import toUpper from 'lodash/toUpper';
 import kebabCase from 'lodash/kebabCase';
 import deburr from 'lodash/deburr';
 import moment from 'moment';
 
-import MapView from 'react-native-maps';
 import CircleButton from 'components/common/circle-button';
 import MapAttribution from 'components/map/map-attribution';
 import BottomDialog from 'components/map/bottom-dialog';
 import LocationErrorBanner from 'components/map/locationErrorBanner';
-import Clusters from 'containers/map/clusters';
-import Basemap from 'containers/map/basemap';
-import RouteMarkers from './route';
 import {
   formatCoordsByFormat,
   formatDistance,
   getDistanceOfLine,
   getMapZoom,
-  getNeighboursSelected,
   getPolygonBoundingBox
 } from 'helpers/map';
 import debounceUI from 'helpers/debounceUI';
 import tracker from 'helpers/googleAnalytics';
-import clusterGenerator from 'helpers/clusters-generator';
 import { LOCATION_TRACKING } from 'config/constants';
 import Theme from 'config/theme';
 import i18n from 'locales';
@@ -484,66 +477,6 @@ class MapComponent extends Component {
     });
   });
 
-  onMapReady = () => {
-    this.forceRefreshLayout();
-    if (this.props.areaCoordinates) {
-      requestAnimationFrame(() => this.map.fitToCoordinates(this.props.areaCoordinates, this.FIT_OPTIONS));
-    }
-    this.props.setActiveAlerts();
-    this.updateMarkers();
-  };
-
-  /**
-   * Makes the compass usable. The mapPadding property of the MapView by default clips/crops the map views instead
-   * of applying the padding and pushing them inwards.
-   *
-   * This method forces the map view to redraw/layout after the padding has been applied and fixes the problem.
-   * GitHub Issues:
-   * https://github.com/react-native-community/react-native-maps/issues/2336
-   * https://github.com/react-native-community/react-native-maps/issues/1033
-   */
-  forceRefreshLayout = () => {
-    if (!this.state.layoutHasForceRefreshed) {
-      this.setState({
-        layoutHasForceRefreshed: true
-      });
-    }
-  };
-
-  getMarkerSize() {
-    const { mapZoom } = this.state;
-    const expandClusterZoomLevel = 15;
-    const initialMarkerSize = 18;
-
-    const scale = mapZoom - expandClusterZoomLevel;
-    const rescaleFactor = mapZoom <= expandClusterZoomLevel ? 1 : scale;
-    const size = initialMarkerSize * rescaleFactor;
-    return { height: size, width: size };
-  }
-
-  updateMarkers = debounce((clean = false) => {
-    const { region, mapZoom } = this.state;
-    const bbox = [
-      region.longitude - region.longitudeDelta / 2,
-      region.latitude - region.latitudeDelta / 2,
-      region.longitude + region.longitudeDelta / 2,
-      region.latitude + region.latitudeDelta / 2
-    ];
-    const clusters = clusterGenerator.clusters && clusterGenerator.clusters.getClusters(bbox, mapZoom);
-    const markers = clusters || [];
-    markers.activeMarkersId = markers.length > 0 ? bbox.join('_') + mapZoom : '';
-
-    if (clean) {
-      this.setState({
-        markers,
-        selectedAlerts: [],
-        neighbours: []
-      });
-    } else {
-      this.setState({ markers });
-    }
-  }, 300);
-
   // Zoom map to user location
   zoomToUserLocation = debounceUI(() => {
     const { lastPosition } = this.state;
@@ -610,6 +543,7 @@ class MapComponent extends Component {
     });
   };
 
+  // todo reimplement onMapDragging functionality
   onRegionChangeComplete = region => {
     const mapZoom = getMapZoom(region);
 
@@ -617,62 +551,12 @@ class MapComponent extends Component {
       prevState => ({
         region,
         mapZoom,
-        selectedAlerts: prevState.customReporting && prevState.dragging ? [region] : prevState.selectedAlerts,
         dragging: false
       }),
       () => {
         this.updateMarkers();
       }
     );
-  };
-
-  zoomTo = (coordinates, id) => {
-    // We substract one so there's always some margin
-    const zoomScale = clusterGenerator.clusters.getClusterExpansionZoom(id) - 1;
-    const zoomCoordinates = {
-      latitude: coordinates.latitude,
-      longitude: coordinates.longitude,
-      latitudeDelta: this.state.region.latitudeDelta / zoomScale,
-      longitudeDelta: this.state.region.longitudeDelta / zoomScale
-    };
-    this.map.animateToRegion(zoomCoordinates);
-  };
-
-  selectAlert = coordinate => {
-    if (coordinate && !this.state.customReporting) {
-      this.setState(prevState => ({
-        neighbours: getNeighboursSelected([...prevState.selectedAlerts, coordinate], prevState.markers),
-        selectedAlerts: [...prevState.selectedAlerts, coordinate]
-      }));
-    }
-  };
-
-  removeSelection = coordinate => {
-    this.setState(state => {
-      let neighbours = [];
-      if (state.selectedAlerts && state.selectedAlerts.length > 0) {
-        const selectedAlerts = state.selectedAlerts.filter(
-          alert => alert.latitude !== coordinate.latitude || alert.longitude !== coordinate.longitude
-        );
-        neighbours = selectedAlerts.length > 0 ? getNeighboursSelected(selectedAlerts, state.markers) : [];
-        return {
-          neighbours,
-          selectedAlerts
-        };
-      }
-      return { selectedAlerts: [] };
-    });
-  };
-
-  includeNeighbour = coordinate => {
-    this.setState(state => {
-      const selectedAlerts = [...state.selectedAlerts, coordinate];
-      const neighbours = getNeighboursSelected(selectedAlerts, state.markers);
-      return {
-        neighbours,
-        selectedAlerts
-      };
-    });
   };
 
   updateSelectedArea = () => {
@@ -822,146 +706,11 @@ class MapComponent extends Component {
     ) : null;
   }
 
-  onMoveShouldSetResponder = () => {
-    // Hack to fix onPanDrag not working for iOS when scroll enabled
-    // https://github.com/react-community/react-native-maps/blob/master/docs/mapview.md
-    this.setState({ dragging: true });
-    return false;
-  };
-
   render() {
-    const { lastPosition, customReporting, selectedAlerts, neighbours, heading, markers } = this.state;
-
-    const {
-      areaCoordinates,
-      area,
-      contextualLayer,
-      isConnected,
-      route,
-      isOfflineMode,
-      ctxLayerLocalTilePath
-    } = this.props;
-    const hasAlertsSelected = selectedAlerts && selectedAlerts.length > 0;
-    const showCompassLine = lastPosition && hasAlertsSelected;
-    const isIOS = Platform.OS === 'ios';
-    const ctxLayerKey =
-      isIOS && contextualLayer ? `contextualLayerElement-${contextualLayer.name}` : 'contextualLayerElement';
-    const keyRand = isIOS ? Math.floor(Math.random() * 100 + 1) : '';
-    const clustersKey = markers
-      ? `clustersElement-${clusterGenerator.activeClusterId}_${markers.activeMarkersId}`
-      : 'clustersElement';
-    const markerSize = this.getMarkerSize();
-    const markerBorder = { borderWidth: (markerSize.width / 18) * 4 };
+    const { customReporting } = this.state;
+    const { isConnected, isOfflineMode } = this.props;
 
     // Map elements
-
-    const contextualLocalLayerElement = ctxLayerLocalTilePath ? (
-      <MapView.LocalTile
-        key={`${ctxLayerKey}_local`}
-        pathTemplate={ctxLayerLocalTilePath}
-        zIndex={1}
-        maxZoom={12}
-        tileSize={256}
-      />
-    ) : null;
-    const contextualRemoteLayerElement =
-      contextualLayer && !isOfflineMode ? ( // eslint-disable-line
-        <MapView.UrlTile key={ctxLayerKey} urlTemplate={contextualLayer.url} zIndex={2} />
-      ) : null;
-    const compassLineElement = showCompassLine ? (
-      <MapView.Polyline
-        key="compassLineElement"
-        coordinates={[lastPosition, selectedAlerts[selectedAlerts.length - 1]]}
-        strokeColor={Theme.colors.white}
-        strokeWidth={3}
-        zIndex={3}
-      />
-    ) : null;
-    const areaPolygonElement = areaCoordinates ? (
-      <MapView.Polyline
-        key="areaPolygonElement"
-        coordinates={areaCoordinates}
-        strokeColor={Theme.colors.turtleGreen}
-        strokeWidth={3}
-        zIndex={3}
-      />
-    ) : null;
-
-    // In the userPositionElement, if we do not track view changes on iOS the position marker will not render.
-    // However, on Android it needs to be set to false as to resolve memory leaks. In the future, we need to revisit this and hopefully we can remove it altogether.
-    const userPositionElement = lastPosition ? (
-      <MapView.Marker.Animated
-        key="userPositionElement"
-        image={markerImage}
-        coordinate={lastPosition}
-        style={{ zIndex: 10 }}
-        anchor={{ x: 0.5, y: 0.5 }}
-        pointerEvents={'none'}
-        tracksViewChanges={Platform.OS === 'ios' ? true : false}
-      />
-    ) : null;
-    const compassElement =
-      lastPosition && heading !== null ? (
-        <MapView.Marker
-          key="compassElement"
-          coordinate={lastPosition}
-          zIndex={11}
-          anchor={{ x: 0.5, y: 0.5 }}
-          pointerEvents={'none'}
-        >
-          <Image
-            style={{
-              width: 94,
-              height: 94,
-              transform: [{ rotate: `${heading || '0'}deg` }]
-            }}
-            source={compassImage}
-          />
-        </MapView.Marker>
-      ) : null;
-    const neighboursAlertsElement =
-      neighbours && neighbours.length > 0
-        ? neighbours.map((neighbour, i) => (
-            <MapView.Marker
-              key={`neighboursAlertsElement-${i}-${keyRand}`}
-              coordinate={neighbour}
-              anchor={{ x: 0.5, y: 0.5 }}
-              onPress={() => this.includeNeighbour(neighbour)}
-              zIndex={10}
-              tracksViewChanges={false}
-            >
-              <View style={[markerSize, markerBorder, styles.markerIconArea]} />
-            </MapView.Marker>
-          ))
-        : null;
-    const selectedAlertsElement =
-      hasAlertsSelected && !customReporting
-        ? selectedAlerts.map((alert, i) => (
-            <MapView.Marker
-              key={`selectedAlertsElement-${i}-${keyRand}`}
-              coordinate={alert}
-              anchor={{ x: 0.5, y: 0.5 }}
-              onPress={() => this.removeSelection(alert)}
-              zIndex={20}
-              tracksViewChanges={false}
-            >
-              <View style={[markerSize, markerBorder, styles.selectedMarkerIcon]} />
-            </MapView.Marker>
-          ))
-        : null;
-
-    const clustersElement =
-      area && area.dataset ? (
-        <Clusters
-          key={clustersKey}
-          markers={markers}
-          selectAlert={this.selectAlert}
-          zoomTo={this.zoomTo}
-          datasetSlug={area.dataset.slug}
-          markerSize={markerSize}
-        />
-      ) : null;
-
     const customReportingElement = customReporting ? (
       <View
         pointerEvents="none"
@@ -1001,42 +750,12 @@ class MapComponent extends Component {
             <Text style={styles.coordinateText}>{this.getCoordinateAndDistanceText()}</Text>
           </SafeAreaView>
         </View>
-        {/*
-        <MapView
-          ref={ref => {
-            this.map = ref;
-          }}
-          style={{
-            ...styles.map,
-            bottom: styles.map.bottom - this.state.bottomSafeAreaInset
-          }}
-          provider={MapView.PROVIDER_GOOGLE}
-          mapPadding={Platform.OS === 'android' ? { top: 40, bottom: 0, left: 0, right: 0 } : undefined}
-          mapType="none"
-          minZoomLevel={2}
-          maxZoomLevel={18}
-          showsCompass
-          rotateEnabled
-          moveOnMarkerPress={false}
-          onMapReady={this.onMapReady}
-          onRegionChangeComplete={this.onRegionChangeComplete}
-        >
-          <Basemap areaId={area.id} />
-          {contextualLocalLayerElement}
-          {contextualRemoteLayerElement}
-          {clustersElement}
-          {compassLineElement}
-          <RouteMarkers isTracking={this.isRouteTracking()} lastPosition={lastPosition} route={route} />
-          {areaPolygonElement}
-          {neighboursAlertsElement}
-          {selectedAlertsElement}
-          {userPositionElement}
-          {compassElement}
-        </MapView>
-        */}
         <MapboxGL.MapView style={{ flex: 1 }} styleURL={MapboxGL.StyleURL.SatelliteStreet}>
           {userLocationElement}
           {mapCameraElement}
+          {/*mpf todo area polygon*/}
+          {/*mpf todo compass line*/}
+          {/*mpf todo route markers*/}
         </MapboxGL.MapView>
         {customReportingElement}
         {this.renderMapFooter()}
