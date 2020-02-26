e8f6b638cba42994b306fcf97d5e31fa708bdd00
Tim Matthews <tim@3sidedcube.com>
2020-02-26 15:26:49 +0000

    Fixed startup crash due to RNN 6.0.1 now supporting autolinking

--- android/app/src/main/java/com/forestwatcher/MainApplication.java
+++ android/app/src/main/java/com/forestwatcher/MainApplication.java
@@ -1,34 +1,14 @@
 package com.forestwatcher;
 
 import android.content.Context;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
-import com.airbnb.android.react.lottie.LottiePackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
 import com.forestwatcher.intents.IntentsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.mapbox.rctmgl.RCTMGLPackage;
 import com.marianhello.bgloc.react.BackgroundGeolocationPackage;
-import com.reactcommunity.rnlocalize.RNLocalizePackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactnativecommunity.cookies.CookieManagerPackage;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
-import com.rnappauth.RNAppAuthPackage;
-import com.rnziparchive.RNZipArchivePackage;
-import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
-import io.invertase.firebase.analytics.ReactNativeFirebaseAnalyticsPackage;
-import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
-import io.sentry.RNSentryPackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -50,27 +30,8 @@ public class MainApplication extends NavigationApplication {
       @SuppressWarnings("UnnecessaryLocalVariable") List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       packages.addAll(Arrays.<ReactPackage>asList(
-          new AsyncStoragePackage(),
           new BackgroundGeolocationPackage(),
-          new CookieManagerPackage(),
-          new FBSDKPackage(),
-          new ImagePickerPackage(),
-          new IntentsPackage(),
-          new LottiePackage(),
-          new MapsPackage(),
-          new NetInfoPackage(),
-          new ReactNativeConfigPackage(),
-          new ReactNativeDialogsPackage(),
-          new ReactNativeFirebaseAnalyticsPackage(),
-          new ReactNativeFirebaseAppPackage(),
-          new RCTMGLPackage(),
-          new RNAppAuthPackage(),
-          new RNCWebViewPackage(),
-          new RNFetchBlobPackage(),
-          new RNLocalizePackage(),
-          new RNSentryPackage(),
-          new RNZipArchivePackage(),
-          new SafeAreaContextPackage()
+          new IntentsPackage()
       ));
       return packages;
     }
