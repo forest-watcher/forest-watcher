da26a31bca4b5da686dac82d7d2eb22f23d9621c
Tim Matthews <tim@3sidedcube.com>
2020-02-27 15:15:39 +0000

    Added file system logic to create standalone bundle

--- app/helpers/sharing/exportBundle.js
+++ app/helpers/sharing/exportBundle.js
@@ -1,6 +1,9 @@
 // @flow
-import type { ExportBundleRequest, UnpackedSharingBundle } from 'types/sharing.types';
-import FWError from 'helpers/fwError';
+import type { State } from 'types/store.types';
+import type { ExportBundleRequest, SharingBundle, UnpackedSharingBundle } from 'types/sharing.types';
+import RNFS from 'react-native-fs';
+import { zip } from 'react-native-zip-archive';
+
 import deleteStagedBundle from 'helpers/sharing/deleteStagedBundle';
 
 /**
@@ -11,12 +14,27 @@ import deleteStagedBundle from 'helpers/sharing/deleteStagedBundle';
 export const BUNDLE_FORMAT_VERSION: number = 1;
 
 /**
+ * Extension of the final bundle
+ */
+export const BUNDLE_FILE_EXTENSION: string = '.gfw.zip';
+
+/**
+ * Directory on the device where the bundle will be prepared
+ */
+export const BUNDLE_DEFAULT_STAGING_DIR: string = RNFS.TemporaryDirectoryPath;
+
+/**
+ * The name of the data file held in the root of a bundle archive
+ */
+export const BUNDLE_DATA_FILE_NAME: string = 'bundle.json';
+
+/**
  * Exports a subset of app data to a standalone file that can be imported into other instances of the FW app
  *
  * @param request - The request defining which data should be exported
  */
-export default async function exportBundle(request: ExportBundleRequest): Promise<File> {
-  const stagedBundle = await stageBundle(request);
+export default async function exportBundle(request: ExportBundleRequest, appState: State): Promise<string> {
+  const stagedBundle = await stageBundle(request, appState);
   const bundleFile = await packageBundle(stagedBundle);
   deleteStagedBundle(stagedBundle);
   return bundleFile;
@@ -27,8 +45,9 @@ export default async function exportBundle(request: ExportBundleRequest): Promis
  *
  * @param bundle - The staged bundle to pack
  */
-export function packageBundle(bundle: UnpackedSharingBundle): Promise<File> {
-  throw new FWError('Not yet implemented');
+export async function packageBundle(bundle: UnpackedSharingBundle): Promise<string> {
+  const outputFilePath = `${bundle.path}${BUNDLE_FILE_EXTENSION}`;
+  return await zip(bundle.path, outputFilePath);
 }
 
 /**
@@ -36,6 +55,28 @@ export function packageBundle(bundle: UnpackedSharingBundle): Promise<File> {
  *
  * @param request - The request defining which data should be exported
  */
-export function stageBundle(request: ExportBundleRequest): Promise<UnpackedSharingBundle> {
-  throw new FWError('Not yet implemented');
+export async function stageBundle(request: ExportBundleRequest, appState: State): Promise<UnpackedSharingBundle> {
+  const areaIds = request.areaIds;
+  const areas = areaIds.map(id => appState.areas.data.find(area => area.id === id)).filter(Boolean);
+
+  const outputPath = `${BUNDLE_DEFAULT_STAGING_DIR}/bundle-${Date.now().toString()}`;
+
+  const bundle: SharingBundle = {
+    version: BUNDLE_FORMAT_VERSION,
+    areas: areas,
+    reports: []
+  };
+  await writeBundle(bundle, outputPath);
+
+  return {
+    path: outputPath,
+    data: bundle
+  };
+}
+
+async function writeBundle(bundle: SharingBundle, outputPath: string): Promise<string> {
+  const outputFile = `${outputPath}/${BUNDLE_DATA_FILE_NAME}`;
+  const outputData = JSON.stringify(bundle);
+  await RNFS.writeFile(outputFile, outputData);
+  return outputFile;
 }
