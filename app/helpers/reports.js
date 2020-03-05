6f8cf4234ec148e5c92df41689016cb5bd8f54cf
Simon Mitchell <simon@3sidedcube.com>
2020-03-05 14:53:39 +0000

    Moves report name regex to non-global-scope const to fix bug where it would only work every 2n times!

--- app/helpers/reports.js
+++ app/helpers/reports.js
@@ -2,7 +2,6 @@
 
 import i18n from 'i18next';
 
-const reportNameRegex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/g;
 
 /**
  * Converts a report name, to a readable, localised string
@@ -11,7 +10,8 @@ const reportNameRegex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/g;
  */
 export function readableNameForReportName(reportName: string) {
   // Using a full regex match for the end of the report name here including data so if user names there area something like SAMS-WIGGLY-REPORT
-  // it doesn't intefere with our logic
+  // it doesn't intefere with our logic. Can't be a global const otherwise we get a weird issue where it fails every n+1 attempts and returns null!
+  const reportNameRegex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/g;
   const result = reportNameRegex.exec(reportName);
   if (!result || result.length < 2) {
     return reportName;
