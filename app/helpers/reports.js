f16a42b601f872f27d8bda1c099c717175680001
Simon Mitchell <simon@3sidedcube.com>
2020-03-06 09:16:15 +0000

    Changes back report name regex to be global Uses match rather than exec Removes global flag from regex

--- app/helpers/reports.js
+++ app/helpers/reports.js
@@ -2,16 +2,18 @@
 
 import i18n from 'i18next';
 
+ // Using a full regex match for the end of the report name here including data so if user names there area something like SAMS-WIGGLY-REPORT
+  // it doesn't intefere with our logic.
+const reportNameRegex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/;
+
 /**
  * Converts a report name, to a readable, localised string
  * @param {string} reportName - The report name to convert
  * @return {string} A localised, readable version of the string
  */
 export function readableNameForReportName(reportName: string) {
-  // Using a full regex match for the end of the report name here including data so if user names there area something like SAMS-WIGGLY-REPORT
-  // it doesn't intefere with our logic. Can't be a global const otherwise we get a weird issue where it fails every n+1 attempts and returns null!
-  const reportNameRegex = /-([A-Z]+)-REPORT--\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d/g;
-  const result = reportNameRegex.exec(reportName);
+  const result = reportName.match(reportNameRegex);
+  console.log("Result!", result);
   if (!result || result.length < 2) {
     return reportName;
   }
