ef3e527df2aec7f54cd8b8690e0ed67e475a01ec
Michael P-F <michael@3sidedcube.com>
2020-03-13 10:54:46 +0000

    Add comment to weird style changes.

--- app/components/common/area-list/styles.js
+++ app/components/common/area-list/styles.js
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
+    // GFW-459 - prevent area tooltip bottom from being cut off on android
     marginBottom: 76,
     marginTop: -50
   }
