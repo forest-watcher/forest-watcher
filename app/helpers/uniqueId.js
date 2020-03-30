ea6196f28f667b97fe190d1015c6baada62cb4a4
Simon Mitchell <simon@3sidedcube.com>
2020-03-30 16:35:54 +0100

    Lints project

--- app/helpers/uniqueId.js
+++ app/helpers/uniqueId.js
@@ -2,7 +2,7 @@ import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
- * Generates a unique ID based on the current date and a random 
+ * Generates a unique ID based on the current date and a random
  *
  * @return {String}
  */
