6a59fccaf1995801af326c0b41f38770ee461b3f
Michael P-F <michael@3sidedcube.com>
2020-03-30 10:43:48 +0100

    Mocking mapbox dependency for tests. Updated test snapshots.

---
+++ __mocks__/@react-native-mapbox-gl/maps.ts
@@ -0,0 +1,12 @@
+export default {
+  StyleURL: {
+    Street: 'mapbox://styles/mapbox/streets-v11',
+    Dark: 'mapbox://styles/mapbox/dark-v10',
+    Light: 'mapbox://styles/mapbox/light-v10',
+    Outdoors: 'mapbox://styles/mapbox/outdoors-v11',
+    Satellite: 'mapbox://styles/mapbox/satellite-v9',
+    SatelliteStreet: 'mapbox://styles/mapbox/satellite-streets-v11',
+    TrafficDay: 'mapbox://styles/mapbox/navigation-preview-day-v4',
+    TrafficNight: 'mapbox://styles/mapbox/navigation-preview-night-v4',
+  }
+};
