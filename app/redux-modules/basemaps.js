63680e8a5647a7b8f671345cce2ffa842df53f30
Michael P-F <michael@3sidedcube.com>
2020-04-15 17:04:04 +0100

    Update default basemaps to GFW mapbox styles. (Work in progress - not complete)

--- app/redux-modules/basemaps.js
+++ app/redux-modules/basemaps.js
@@ -1,16 +1,27 @@
 // @flow
 import type { Basemap, BasemapsAction, BasemapsState } from 'types/basemaps.types';
-import MapboxGL from '@react-native-mapbox-gl/maps';
 
 // Actions
 const ADD_BASEMAP = 'basemaps/ADD_BASEMAP';
 
 // Constants
 const allDefaultMapboxStyles = [
-  [MapboxGL.StyleURL.Satellite, 'Satellite', require('assets/basemap_satellite.png')],
-  [MapboxGL.StyleURL.Light, 'Light', require('assets/basemap_light.png')],
-  [MapboxGL.StyleURL.Dark, 'Dark', require('assets/basemap_dark.png')],
-  [MapboxGL.StyleURL.Street, 'Street', require('assets/basemap_street.png')]
+  [
+    'mapbox://styles/resourcewatch/cjww7iv8i07yx1cmjtgazn3r0?fresh=true',
+    'Default',
+    require('assets/basemap_default.png')
+  ],
+  ['mapbox://styles/resourcewatch/cjww836hy1kep1co5xp717jek?fresh=true', 'Dark', require('assets/basemap_dark.png')],
+  [
+    'mapbox://styles/resourcewatch/cjww89e5j08o91cmjsbrd47qt?fresh=true',
+    'Satellite',
+    require('assets/basemap_satellite.png')
+  ],
+  [
+    'mapbox://styles/resourcewatch/cjww8drml27wc1cn3mk2872h9?fresh=true',
+    'Landsat',
+    require('assets/basemap_landsat.png')
+  ]
 ];
 
 const mapboxBasemaps: Array<Basemap> = allDefaultMapboxStyles.map(style => {
