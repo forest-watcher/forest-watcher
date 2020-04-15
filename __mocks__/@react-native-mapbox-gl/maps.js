866077cda76aeaa0e3ed6ecdc2ec0f8de054c0a6
Michael P-F <michael@3sidedcube.com>
2020-04-15 17:22:43 +0100

    Update tests and remove test mock file.

--- __mocks__/@react-native-mapbox-gl/maps.js
+++
@@ -1,8 +0,0 @@
-export default {
-  StyleURL: {
-    Satellite: 'mapbox://styles/mapbox/satellite-v9',
-    Light: 'mapbox://styles/mapbox/light-v10',
-    Dark: 'mapbox://styles/mapbox/dark-v10',
-    Street: 'mapbox://styles/mapbox/streets-v11'
-  }
-};
