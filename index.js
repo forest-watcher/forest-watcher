38344350ca93f329b3863246dffa85708a5408b5
Simon Mitchell <simon@3sidedcube.com>
2020-04-28 13:37:00 +0100

    Adds status bar fix for iOS 13

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { Platform, UIManager } from 'react-native';
+import { Platform, UIManager, StatusBar } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import App from './app/main';
 
 // Don't enable animation support on Android, as it was causing strange UI issues (see https://3sidedcube.atlassian.net/browse/GFW-370)
 if (Platform.OS === 'android') {
   UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(false);
+}
+
+if (Platform.OS === 'ios') {
+	StatusBar.setBarStyle('dark-content');
 }
 
 const app = new App();
