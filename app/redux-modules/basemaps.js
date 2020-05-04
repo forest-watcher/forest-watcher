8c57ef503eb4e24b919ea51f53ace757e765ae63
Michael P-F <michael@3sidedcube.com>
2020-05-04 10:21:52 +0100

    Write basemaps as objects.

--- app/redux-modules/basemaps.js
+++ app/redux-modules/basemaps.js
@@ -5,37 +5,40 @@ import type { Basemap, BasemapsAction, BasemapsState } from 'types/basemaps.type
 const ADD_BASEMAP = 'basemaps/ADD_BASEMAP';
 
 // Constants
-const allDefaultMapboxStyles = [
-  [
-    'mapbox://styles/resourcewatch/cjww7iv8i07yx1cmjtgazn3r0?fresh=true',
-    'Default',
-    require('assets/basemap_default.png')
-  ],
-  ['mapbox://styles/resourcewatch/cjww836hy1kep1co5xp717jek?fresh=true', 'Dark', require('assets/basemap_dark.png')],
-  [
-    'mapbox://styles/resourcewatch/cjww89e5j08o91cmjsbrd47qt?fresh=true',
-    'Satellite',
-    require('assets/basemap_satellite.png'),
-    'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
-  ],
-  [
-    'mapbox://styles/resourcewatch/cjww8drml27wc1cn3mk2872h9?fresh=true',
-    'Landsat',
-    require('assets/basemap_landsat.png'),
-    'https://production-api.globalforestwatch.org/v2/landsat-tiles/2017/{z}/{x}/{y}'
-  ]
-];
-
-const mapboxBasemaps: Array<Basemap> = allDefaultMapboxStyles.map(style => {
-  return {
+const mapboxBasemaps: Array<Basemap> = [
+  {
     isMapboxStyle: true,
-    styleURL: style[0],
-    id: style[0],
-    name: style[1],
-    image: style[2],
-    tileUrl: style.length > 3 ? style[3] : undefined
-  };
-});
+    id: 'mapbox://styles/resourcewatch/cjww7iv8i07yx1cmjtgazn3r0?fresh=true',
+    styleURL: 'mapbox://styles/resourcewatch/cjww7iv8i07yx1cmjtgazn3r0?fresh=true',
+    name: 'Default',
+    image: require('assets/basemap_default.png'),
+    tileUrl: null
+  },
+  {
+    isMapboxStyle: true,
+    id: 'mapbox://styles/resourcewatch/cjww836hy1kep1co5xp717jek?fresh=true',
+    styleURL: 'mapbox://styles/resourcewatch/cjww836hy1kep1co5xp717jek?fresh=true',
+    name: 'Dark',
+    image: require('assets/basemap_dark.png'),
+    tileUrl: null
+  },
+  {
+    isMapboxStyle: true,
+    id: 'mapbox://styles/resourcewatch/cjww89e5j08o91cmjsbrd47qt?fresh=true',
+    styleURL: 'mapbox://styles/resourcewatch/cjww89e5j08o91cmjsbrd47qt?fresh=true',
+    name: 'Satellite',
+    image: require('assets/basemap_satellite.png'),
+    tileUrl: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+  },
+  {
+    isMapboxStyle: true,
+    id: 'mapbox://styles/resourcewatch/cjww8drml27wc1cn3mk2872h9?fresh=true',
+    styleURL: 'mapbox://styles/resourcewatch/cjww8drml27wc1cn3mk2872h9?fresh=true',
+    name: 'Landsat',
+    image: require('assets/basemap_landsat.png'),
+    tileUrl: 'https://production-api.globalforestwatch.org/v2/landsat-tiles/2017/{z}/{x}/{y}'
+  }
+];
 
 export const DEFAULT_BASEMAP = mapboxBasemaps[0];
 
