a61514b6816621bc4c6c98245483d21e9b0cd0e9
Tim Matthews <tim@3sidedcube.com>
2020-05-07 15:20:20 +0100

    Setup layer store to support custom contextual layers

--- app/helpers/sharing/manifestBundleSize.js
+++ app/helpers/sharing/manifestBundleSize.js
@@ -1,33 +1,16 @@
 // @flow
 
-import type { LayerFile, LayerManifest } from 'types/sharing.types';
+import type { LayerFile, SharingBundleManifest } from 'types/sharing.types';
 
 /**
  * Returns a rough approximation of the export file size for a given export layer manifest
  */
-export default function manifestBundleSize(manifest: LayerManifest): number {
+export default function manifestBundleSize(manifest: SharingBundleManifest): number {
   let size = 0;
-  const basemapFiles: Array<LayerFile> = Object.keys(manifest.basemaps)
-    .map(key => manifest.basemaps[key])
-    .flatMap(element => {
-      return element;
-    });
-  const layerFiles: Array<LayerFile> = Object.keys(manifest.layers)
-    .map(key => manifest.layers[key])
-    .flatMap(element => {
-      return element;
-    });
-
-  const allFiles = basemapFiles.concat(layerFiles);
-
-  allFiles.forEach((file: LayerFile) => {
-    if (file.filesize) {
-      const fileSize = parseInt(file.filesize);
-      if (!isNaN(fileSize)) {
-        size += fileSize;
-      }
+  manifest.layerFiles.forEach((file: LayerFile) => {
+    if (!isNaN(file.size)) {
+      size += file.size;
     }
   });
-
   return size;
 }
