34840ac963d042c9ae760d901c740bbf4297e96f
Tim Matthews <tim@3sidedcube.com>
2020-06-04 17:48:36 +0100

    Updated Cancel button style

--- app/components/sharing-bundle/import/custom-items/index.js
+++ app/components/sharing-bundle/import/custom-items/index.js
@@ -38,8 +38,7 @@ export default class ImportSharingBundleCustomItemsScreen extends PureComponent<
           {
             id: 'cancel',
             text: i18n.t('commonText.cancel'),
-            color: Theme.colors.turtleGreen,
-            fontFamily: Theme.font
+            ...styles.topBarTextButton
           }
         ],
         title: {
