f38d6f797972ccebe1d8b6754893dca8b01ee8a8
Ryan Bourne <ryan@peslostudios.com>
2020-06-12 12:21:17 +0100

    GFW-467 - removed console.warns, updated snapshots

--- app/helpers/analytics.js
+++ app/helpers/analytics.js
@@ -21,32 +21,24 @@ export const disableAnalytics = (disabled: boolean) => {
 };
 
 export const trackLogin = (method: ?LoginMethod) => {
-  console.warn(`trackLogin - ${method}`);
-
   analytics().logLogin({
     method: method ?? 'unknown'
   });
 };
 
 export const trackMenuButtonPress = (menuButton: MenuButtonType) => {
-  console.warn(`trackMenuButtonPress - ${menuButton}`);
-
   analytics().logEvent('menu_button_tap', {
     screen_name: menuButton
   });
 };
 
 export const trackScreenView = (screenName: string) => {
-  console.warn(`trackScreenView - ${screenName}`);
-
   analytics().setCurrentScreen(screenName, screenName);
 };
 
 /// AREA
 
 export const trackAreaCreationFlowStarted = () => {
-  console.warn(`trackAreaCreationFlowStarted`);
-
   analytics().logEvent('area_creation_start');
 };
 
@@ -54,7 +46,6 @@ export const trackAreaCreationFlowStarted = () => {
 // we also round it 3dp so firebase is fine with it
 export const trackAreaCreationFlowEnded = (areaSize: number) => {
   const kmSquaredValue = parseFloat((areaSize / 1000000).toFixed(3));
-  console.warn(`trackAreaCreationFlowEnded - ${kmSquaredValue}`);
 
   analytics().logEvent('area_creation_end', {
     value: kmSquaredValue
@@ -65,7 +56,6 @@ const areaDownloadTimers: { [id: string]: number } = {};
 
 export const trackAreaDownloadFlowStarted = (areaId: string) => {
   areaDownloadTimers[areaId] = Date.now();
-  console.warn(`trackAreaDownloadFlowStarted - ${areaId}`);
 
   analytics().logEvent('area_download_start');
 };
@@ -73,7 +63,6 @@ export const trackAreaDownloadFlowStarted = (areaId: string) => {
 export const trackAreaDownloadFlowEnded = (areaId: string, success: boolean) => {
   const duration = areaDownloadTimers[areaId] ? Math.ceil((Date.now() - areaDownloadTimers[areaId]) / 1000) : 0;
   delete areaDownloadTimers[areaId];
-  console.warn(`trackAreaDownloadFlowEnded - ${areaId} ${duration} ${success}`);
 
   analytics().logEvent('area_download_end', {
     time_taken: duration,
@@ -94,8 +83,6 @@ export const trackImportedContent = (
     .pop()
     .toLowerCase();
 
-  console.warn(`trackImportedContent - ${contentType} ${fileType} ${success} ${formatBytes(fileSize ?? 0)}`);
-
   analytics().logEvent('imported_content', {
     content_type: contentType,
     file_type: fileType,
@@ -114,7 +101,6 @@ export const trackContentDownloadStarted = (id: string) => {
 export const trackDownloadedContent = (contentType: DownloadableContentType, id: string, success: boolean) => {
   const duration = contentDownloadTimers[id] ? Math.ceil((Date.now() - contentDownloadTimers[id]) / 1000) : 0;
   delete contentDownloadTimers[id];
-  console.warn(`trackDownloadedContent - ${contentType} ${id} ${duration} ${success}`);
 
   analytics().logEvent('downloaded_content', {
     content_type: contentType,
@@ -126,8 +112,6 @@ export const trackDownloadedContent = (contentType: DownloadableContentType, id:
 /// EXPORT
 
 export const trackSharedContent = (contentType: SharableContentType) => {
-  console.warn(`trackSharedContent - ${contentType}`);
-
   analytics().logEvent('exported_content', {
     content_type: contentType
   });
@@ -136,8 +120,6 @@ export const trackSharedContent = (contentType: SharableContentType) => {
 /// MAP LAYERS
 
 export const trackRoutesToggled = (routeTypes: 'all' | 'some' | 'none', routesTotal: number, enabled: boolean) => {
-  console.warn(`trackRoutesToggled - ${routeTypes} ${routesTotal} ${enabled}`);
-
   analytics().logEvent('routes_toggled', {
     layer_name: routeTypes,
     value: routesTotal,
@@ -146,8 +128,6 @@ export const trackRoutesToggled = (routeTypes: 'all' | 'some' | 'none', routesTo
 };
 
 export const trackLayersToggled = (layerId: ?string, enabled: boolean) => {
-  console.warn(`trackLayersToggled - ${layerId} ${enabled}`);
-
   analytics().logEvent('layer_toggled', {
     layer_id: layerId ?? 'unknown',
     layer_enabled: enabled ? 1 : 0
@@ -155,8 +135,6 @@ export const trackLayersToggled = (layerId: ?string, enabled: boolean) => {
 };
 
 export const trackAlertTypeToggled = (alertType: AlertType, enabled: boolean) => {
-  console.warn(`trackAlertTypeToggled - ${alertType} ${enabled}`);
-
   analytics().logEvent('alert_type_toggled', {
     layer_name: alertType,
     layer_enabled: enabled ? 1 : 0
@@ -164,8 +142,6 @@ export const trackAlertTypeToggled = (alertType: AlertType, enabled: boolean) =>
 };
 
 export const trackReportsToggled = (reportType: ImportableLayerCategories, enabled: boolean) => {
-  console.warn(`trackReportsToggled - ${reportType} ${enabled}`);
-
   analytics().logEvent('reports_toggled', {
     layer_name: reportType,
     layer_enabled: enabled ? 1 : 0
@@ -175,8 +151,6 @@ export const trackReportsToggled = (reportType: ImportableLayerCategories, enabl
 /// ROUTES
 
 export const trackRouteFlowEvent = (routingAction: RoutingAction) => {
-  console.warn(`trackRouteFlowEvent - ${routingAction}`);
-
   analytics().logEvent('route_action', {
     action: routingAction
   });
@@ -190,7 +164,6 @@ export const trackRouteDetailsUpdated = (
 ) => {
   const durationInSeconds = parseInt(timeTaken / 1000);
   const distanceInKm = parseFloat((length / 1000).toFixed(3));
-  console.warn(`trackRouteDetailsUpdated - ${difficulty} ${durationInSeconds} ${date} ${distanceInKm}`);
 
   analytics().logEvent('route_details_updated', {
     difficulty: difficulty,
@@ -204,7 +177,6 @@ const routeDownloadTimers: { [id: string]: number } = {};
 
 export const trackRouteDownloadFlowStarted = (routeId: string) => {
   routeDownloadTimers[routeId] = Date.now();
-  console.warn(`trackRouteDownloadFlowStarted - ${routeId}`);
 
   analytics().logEvent('route_download_start');
 };
@@ -212,7 +184,6 @@ export const trackRouteDownloadFlowStarted = (routeId: string) => {
 export const trackRouteDownloadFlowEnded = (routeId: string, success: boolean) => {
   const duration = routeDownloadTimers[routeId] ? Math.ceil((Date.now() - routeDownloadTimers[routeId]) / 1000) : 0;
   delete routeDownloadTimers[routeId];
-  console.warn(`trackRouteDownloadFlowEnded - ${routeId} ${duration} ${success}`);
 
   analytics().logEvent('route_download_end', {
     time_taken: duration,
@@ -225,7 +196,6 @@ export const trackRouteDownloadFlowEnded = (routeId: string, success: boolean) =
 let startReportingTimestamp: ?number = null;
 
 export const trackReportingStarted = (totalAlerts: number, source: ReportingSource) => {
-  console.warn(`trackReportingStarted - ${totalAlerts} ${source}`);
   startReportingTimestamp = Date.now();
 
   analytics().logEvent('report_start', {
@@ -236,7 +206,6 @@ export const trackReportingStarted = (totalAlerts: number, source: ReportingSour
 
 export const trackReportingConcluded = (reportOutcome: ReportingOutcome, screenName: string) => {
   const timeTaken = startReportingTimestamp != null ? Math.ceil((Date.now() - startReportingTimestamp) / 1000) : 0;
-  console.warn(`trackReportingConcluded - ${reportOutcome} ${screenName} ${timeTaken}`);
   startReportingTimestamp = null;
 
   analytics().logEvent('report_end', {
