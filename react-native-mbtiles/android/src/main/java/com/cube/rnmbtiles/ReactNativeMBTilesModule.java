552a28169ea3481fb21d6d8386ad783eacfb452e
Tim Matthews <tim@3sidedcube.com>
2020-06-17 21:20:45 +0100

    Use nanohttpd as HTTP server for Android MBTiles implementation

--- react-native-mbtiles/android/src/main/java/com/cube/rnmbtiles/ReactNativeMBTilesModule.java
+++ react-native-mbtiles/android/src/main/java/com/cube/rnmbtiles/ReactNativeMBTilesModule.java
@@ -7,6 +7,9 @@ import com.facebook.react.bridge.ReactMethod;
 
 public class ReactNativeMBTilesModule extends ReactContextBaseJavaModule {
 
+    private RNMBTileSource source = null;
+    private RNMBTileServer server = null;
+
     public ReactNativeMBTilesModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -20,12 +23,6 @@ public class ReactNativeMBTilesModule extends ReactContextBaseJavaModule {
     public void getMetadata(String basemapId, String basemapPath, Callback callback) {
         try {
             RNMBTileSource source = new RNMBTileSource(basemapId, basemapPath);
-
-            if (null == source) {
-                callback.invoke(true, null);
-                return;
-            }
-
             callback.invoke(null, source.getMappedMetadata());
         } catch (Error e) {
             callback.invoke(true, null);
@@ -35,7 +32,8 @@ public class ReactNativeMBTilesModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void prepare(String basemapId, String basemapPath, Callback callback) {
         try {
-            RNMBTileMetadata metadata = RNMBTileServer.INSTANCE.prepare(basemapId, basemapPath);
+            source = new RNMBTileSource(basemapId, basemapPath);
+            RNMBTileMetadata metadata = source.getMetadata();
 
             if (null == metadata) {
                 callback.invoke(true, null);
@@ -43,18 +41,37 @@ public class ReactNativeMBTilesModule extends ReactContextBaseJavaModule {
             }
 
             callback.invoke(null, metadata.getMappedMetadata());
-        } catch (Error e) {
+        } catch (Exception e) {
             callback.invoke(true, null);
         }
     }
 
     @ReactMethod
     public void startServer(Integer port) {
-        RNMBTileServer.INSTANCE.startServer(port);
+        if (source == null) {
+            return;
+        }
+
+        if (server != null) {
+            throw new RuntimeException("A server is already running");
+        }
+
+        server = new RNMBTileServer(source, port);
+        try {
+            server.start();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     @ReactMethod
     public void stopServer() {
-        RNMBTileServer.INSTANCE.stopServer();
+        if (server == null) {
+            return;
+        }
+
+        server.stop();
+        source = null;
+        server = null;
     }
 }
