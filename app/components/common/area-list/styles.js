014f5ea8d1e9c94b637d42c85835fd3e75ab0cb7
Michael P-F <michael@3sidedcube.com>
2020-06-18 10:05:10 +0100

    GFW-579: Use area height constant and separate out method.

--- app/components/common/area-list/styles.js
+++ app/components/common/area-list/styles.js
@@ -1,11 +1,15 @@
 import { Platform, StyleSheet } from 'react-native';
 
+const areaRowContainerMargin = 26;
+const areaRowHeight = 130;
+export const AREA_ROW_TOTAL_HEIGHT = areaRowContainerMargin + areaRowHeight;
+
 export default StyleSheet.create({
   rowContainer: {
-    marginBottom: 26
+    marginBottom: areaRowContainerMargin
   },
   row: {
-    height: Platform.OS === 'android' ? 130 : undefined // extra 2 for bottomBorder in Row component
+    height: Platform.OS === 'android' ? areaRowHeight : undefined // extra 2 for bottomBorder in Row component
   },
   calloutFirstRowContainer: {
     // GFW-459 & GFW-584 - prevent area tooltip bottom from being cut off on android
