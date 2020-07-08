6c21b24f982fe0a49088553434e15f9e4a3c8142
Ryan Bourne <ryan@peslostudios.com>
2020-07-08 19:33:01 +0100

    GFW-731 - ensured that when Realm emits a storage error, we show an alert

--- app/helpers/alert-store/storeAlerts.js
+++ app/helpers/alert-store/storeAlerts.js
@@ -1,6 +1,9 @@
 // @flow
 
 import type { Alert } from 'types/alerts.types';
+
+import { Alert as RNAlert } from 'react-native';
+import i18n from 'i18next';
 import { generateAlertId, initDb } from 'helpers/alert-store/database';
 
 /**
@@ -10,11 +13,23 @@ import { generateAlertId, initDb } from 'helpers/alert-store/database';
  * [1] https://github.com/realm/realm-js/issues/1518
  */
 export default function storeAlerts(alerts: Array<Alert>): void {
-  const alertsWithIds = alerts.map(alert => (alert.id ? alert : { ...alert, id: generateAlertId(alert) }));
-  const realm = initDb();
-  realm.write(() => {
-    alertsWithIds.forEach(alert => {
-      realm.create('Alert', alert, 'modified');
+  try {
+    const alertsWithIds = alerts.map(alert => (alert.id ? alert : { ...alert, id: generateAlertId(alert) }));
+    const realm = initDb();
+
+    realm.write(() => {
+      alertsWithIds.forEach(alert => {
+        realm.create('Alert', alert, 'modified');
+      });
     });
-  });
+  } catch (error) {
+    if (error.code === 13) {
+      RNAlert.alert(i18n.t('sync.outOfSpace.title'), i18n.t('sync.outOfSpace.message'), [
+        {
+          title: i18n.t('commonText.ok'),
+          style: 'default'
+        }
+      ]);
+    }
+  }
 }
